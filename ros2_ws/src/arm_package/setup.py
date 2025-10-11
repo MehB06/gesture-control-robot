01f9382162e72afcb1e9664abb56654ef29f8dcf
Fadi-Mostefai <fadimostefai@gmail.com>
2025-10-11 12:32:27 +0100

    Created starter ros package

---
+++ ros2_ws/src/arm_package/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'arm_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='fadi-mostefai',
+    maintainer_email='fadi-mostefai@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'arm_movement = arm_package.arm_movement:main'
+        ],
+    },
+)
