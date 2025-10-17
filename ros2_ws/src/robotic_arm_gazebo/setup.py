12ec271769cc11c4d88395cf7e9efe6b20977840
Fadi-Mostefai <fadimostefai@gmail.com>
2025-10-17 17:53:05 +0100

    Restarted - created initial ros2 package

---
+++ ros2_ws/src/robotic_arm_gazebo/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob 
+
+
+package_name = 'robotic_arm_gazebo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='fadi',
+    maintainer_email='fadimostefai@gmail.com',
+    description='ROS2 package for simulating a robotic arm in Gazebo',
+    license='Apache License 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'control_node = robotic_arm_gazebo.control_node\:main',
+        ],
+    },
+)
